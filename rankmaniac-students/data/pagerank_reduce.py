f2a6874dda3c74edd77481ad8d8ad216a24fca2d
Catherine <catherinema828@gmail.com>
2017-02-07 00:36:24 -0800

    updated bugs in mapper and reducer

--- rankmaniac-students/data/pagerank_reduce.py
+++ rankmaniac-students/data/pagerank_reduce.py
@@ -2,10 +2,25 @@
 
 import sys
 
-#
-# This program simply represents the identity function.
-#
+alpha = 0.85
+
+prev = -1
+lst_values = []
 
 for line in sys.stdin:
-    sys.stdout.write(line)
+    temp = line.split('\t')
 
+    if prev == -1:
+    	prev = int(temp[0])
+
+    if prev == int(temp[0]):
+    	lst_values.append(alpha * float(temp[1]) + 1.0 - alpha)
+    else:
+    	sum_vals = sum(lst_values)
+    	sys.stdout.write(str(prev) + '\t' + str(sum(lst_values)) + '\n')
+
+    	lst_values = [float(temp[1])]
+    	prev = int(temp[0])
+
+if len(lst_values) == 1:
+	sys.stdout.write(str(prev) + '\t' + str(sum(lst_values)) + '\n')
