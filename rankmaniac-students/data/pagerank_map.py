d3a120f1d267eeb931524c0397ebddf125783560
Catherine <catherinema828@gmail.com>
2017-02-06 23:25:26 -0800

    finished basic reducer

--- rankmaniac-students/data/pagerank_map.py
+++ rankmaniac-students/data/pagerank_map.py
@@ -10,28 +10,9 @@ for line in sys.stdin:
 	outlinks = outlinks.split(',')
 	outlinks = [int(x) for x in outlinks]
 
-	# return list with all outlinked pages as keys
-	# corresponding values are pagerank of origin page divided by total num of outlinks
-	# from origin page
-	# also append to list pair with key the origin page and value 0
-
 	for link in outlinks:
 		key = link
 		value = curr_pr / len(outlinks)
-		sys.stdout.write(key + '\t' + value)
+		sys.stdout.write(str(key) + '\t' + str(value) + '\n')
 
-
-
-
-
-
-
-
-
-
-
-	print node_id
-	print curr_pr
-	print prev_pr
-	for i in outlinks:
-		print i
+	sys.stdout.write(str(node_id) + '\t0\n')
