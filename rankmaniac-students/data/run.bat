a72c1c6f5e959eae5085b81277d7b66cb20953a7
Shen <caffeinated00@gmail.com>
2017-02-07 09:33:57 -0800

    Delete run.bat

--- rankmaniac-students/data/run.bat
+++
@@ -1,6 +0,0 @@
-ECHO OFF
-FOR /L %%A IN (1,1,10) DO (
-ECHO run
-python pagerank_map.py <output.txt | sort | python pagerank_reduce.py | python process_map.py | sort | python process_reduce.py > output.txt
-)
-PAUSE
