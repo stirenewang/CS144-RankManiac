510b0714bfc09e8803bfabe7f1301869017a518c
Catherine <catherinema828@gmail.com>
2017-02-07 01:05:43 -0800

    finished basic mapper reducer process reducer

--- rankmaniac-students/data/process_reduce.py
+++ rankmaniac-students/data/process_reduce.py
@@ -2,10 +2,29 @@
 
 import sys
 
-#
-# This program simply represents the identity function.
-#
+tuples = []
+
+lines = []
 
 for line in sys.stdin:
-    sys.stdout.write(line)
+    lines.append(line)
 
+    line_tab = line.split('\t')
+    line_info = line_tab[1].split(',')
+
+    node_id = int(line_tab[0])
+    pr = float(line_info[0])
+
+    tuples.append((node_id, pr))
+
+sorted_tuples = sorted(tuples, key=lambda x: x[1])
+sorted_tuples = sorted_tuples[::-1]
+
+iteration = 50
+
+if iteration == 50:
+    for i in range(20):
+        sys.stdout.write('FinalRank:' + str(sorted_tuples[i][1]) + '\t' + str(sorted_tuples[i][0]) + '\n')
+else:
+    for i in range(len(lines)):
+        sys.stdout.write('NodeId:' + lines[i])
