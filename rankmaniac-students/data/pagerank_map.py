165180e41866dcbd5a234958175925891e7e1e81
Ida Huang <ihuangda@gmail.com>
2017-02-07 15:30:13 -0800

    clean up some code

--- rankmaniac-students/data/pagerank_map.py
+++ rankmaniac-students/data/pagerank_map.py
@@ -3,7 +3,7 @@
 import sys
 
 for line in sys.stdin:
-    line_tab = line.split('\t')
+    line_tab = line.strip().split('\t')
     line_info = line_tab[1].split(',')
 
     node_id = int(line_tab[0][7:])
@@ -11,10 +11,7 @@ for line in sys.stdin:
     prev_pr = float(line_info[1])
     outlinks = [int(float(x)) for x in line_info[2:]]
 
-    if '\n' in line_tab[1]:
-        sys.stdout.write(str(node_id) + '\t' + 'p,' + line_tab[1])
-    else:
-        sys.stdout.write(str(node_id) + '\t' + 'p,' + line_tab[1] + '\n')
+    sys.stdout.write(str(node_id) + '\t' + 'p,' + line_tab[1] + '\n')
 
     for link in outlinks:
         key = link
