d91ac94395c90e8ba723bb668b1d44404ea3f41f
Catherine <catherinema828@gmail.com>
2017-02-07 17:03:20 -0800

    got random graph ranking right

--- rankmaniac-students/data/process_reduce.py
+++ rankmaniac-students/data/process_reduce.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
+
 import sys
+
 tuples = []
-iteration = 0
 lines = []
+iteration = 0
+
 for line in sys.stdin:
     if len(sys.argv) == 2 and int(sys.argv[1]) == 50:
             iteration = 50
     else:
         iteration = 0
-    #if line[0] == 'i': # READ ITERATION HERE; this will only be on last file
-    #    iteration = int(line[1]) + 1
 
     lines.append(line)
     line_tab = line.split('\t')
@@ -27,6 +28,5 @@ if iteration == 50:
     for i in range(20):
         sys.stdout.write('FinalRank:' + str(sorted_tuples[i][1]) + '\t' + str(sorted_tuples[i][0]) + '\n')
 else:
-    #sys.stdout.write('i' + str(iteration + 1) + '\n')
     for i in range(len(lines)):
         sys.stdout.write('NodeId:' + lines[i])
