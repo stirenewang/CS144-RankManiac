8154e23b611906022c6f71827674e26fedf5c3ee
Catherine <catherinema828@gmail.com>
2017-02-08 20:00:53 -0800

    fixed inefficiencies in process_reduce

--- rankmaniac-students/data/process_reduce.py
+++ rankmaniac-students/data/process_reduce.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
 import sys
+import numpy as np
+import heapq
+from operator import itemgetter
 
 tuples = []
 lines = []
@@ -9,41 +12,31 @@ tolerance = 0.001
 converged = True
 
 for line in sys.stdin:
-    #if len(sys.argv) == 2 and int(sys.argv[1]) == 50:
-    #        iteration = 50
-    #else:
-    #    iteration = 0
-
     lines.append(line)
-    line_tab = line.split('\t')
+    line_tab = line.strip().split('\t')
     line_info = line_tab[1].split(',')
 
-    if line_tab[0] == "i": # first line
-        iteration = int(line_tab[1]) 
+    if line_tab[0] == "i":
+        iteration = int(line_tab[1])
     else:
-        node_id = int(line_tab[0])
+        node_id = line_tab[0]
         
-        # Check for convergence: 
-        # check prev_pr and curr_pr changes. If they are 
-        # greater than the tolerance, there is no convergence
-        curr_pr = float(line_info[0])
-        prev_pr = float(line_info[1])
+        pr = map(float, line_info[:2])
+        change = abs(pr[0] - pr[1])
 
-        change = abs(curr_pr - prev_pr)
         if change > tolerance:
             converged = False
-        tuples.append((node_id, curr_pr))
 
-if iteration == 50 or converged:
-    sorted_tuples = sorted(tuples, key=lambda x: x[1])[-20:]
-    sorted_tuples = sorted_tuples[::-1]
-    for i in range(20):
-        sys.stdout.write('FinalRank:' + str(sorted_tuples[i][1]) + '\t' + str(sorted_tuples[i][0]) + '\n')
-    sys.exit()
+        tuples.append((node_id, pr[0]))
+
+if iteration == 49 or converged:
+    top = heapq.nlargest(20, tuples, key=itemgetter(1))
+
+    for tup in top:
+        sys.stdout.write('FinalRank:' + tup[0] + '\t' + str(tup[1]) + '\n')
 else:
-    sys.stdout.write("i" + "\t" + str(iteration + 1) + "\n")
-    for i in range(len(lines)):
-        # don't concatenate iteration
-        if not lines[i].startswith('i'):            
-            #print node IDs
-            sys.stdout.write('NodeId:' + lines[i])
+    sys.stdout.write('i' + '\t' + str(iteration + 1) + '\n')
+
+    for lin in lines:
+        if not lin.startswith('i'):
+            sys.stdout.write('NodeId:' + lin)
