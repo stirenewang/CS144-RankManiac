7c8a76874b9a4287889d2f812a08f53174b364e6
amaltariq <amalftariq@gmail.com>
2017-02-08 19:29:19 -0800

    Fixed the shell script so that it'll terminate if FinalRank is outputted

--- rankmaniac-students/data/test_50_runs.sh
+++ rankmaniac-students/data/test_50_runs.sh
@@ -3,11 +3,17 @@
 python pagerank_map.py < input_email.txt | sort | python pagerank_reduce.py | python process_map.py | sort | python process_reduce.py > int_output.txt
 cat int_output.txt > int_input.txt
 
-for i in {1..40}
+for i in {1..49}
 do
+	echo "iter: ${i}"
 	python pagerank_map.py < int_input.txt | sort | python pagerank_reduce.py | python process_map.py | sort | python process_reduce.py > int_output.txt
 
-	cat int_output.txt > int_input.txt
+	if grep -q "FinalRank" int_output.txt
+	then
+		cat int_output.txt > output.txt
+		echo "done after ${i} iterations"
+		break
+	else
+		cat int_output.txt > int_input.txt
+	fi
 done
-
-python pagerank_map.py < int_input.txt | sort | python pagerank_reduce.py | python process_map.py | sort | python process_reduce.py 50 > output.txt
