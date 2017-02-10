a7daf5078400929d2ff87a97830309bf794f8bd3
amaltariq <amalftariq@gmail.com>
2017-02-09 21:57:01 -0800

    cleaning up pagerank_reduce

--- rankmaniac-students/data/pagerank_reduce.py
+++ rankmaniac-students/data/pagerank_reduce.py
@@ -2,10 +2,16 @@
 
 import sys
 
-# pline = [curr_pr, prev_pr, outlinks.....]
-alpha = 0.85
-node_pr = {}    # key: nodeID (string), value is contribution (float)
-graph_data = {}     # key: nodeID (string), value is [pline = curr_pr, prev_pr, outlinks...] (list of strings)
+alpha = 0.85        # page rank calculate
+
+# Dictionary of node page ranks on this iteration
+node_pr = {}        # key: nodeID (string)
+                    # value: contribution (float)
+
+# Dictionary of graph information (pline), indicated by 'p' before data
+# pline are lines that look like 'p',curr_pr,prev_pr, outlinks.....
+graph_data = {}     # key: nodeID (string)
+                    # value: [pline = curr_pr (str), prev_pr (str), outlinks (str)]
 
 
 for line in sys.stdin:
@@ -14,7 +20,7 @@ for line in sys.stdin:
     if line_tab[0] == "i" or line_tab[0] == 'c':    # if line tells iteration or convergence counter
         sys.stdout.write(line)
     else:
-        line_info = line_tab[1].strip().split(',')          # get contribution/pline
+        line_info = line_tab[1].strip().split(',', 3)          # get contribution/pline
         node_id = line_tab[0]                       # get node ID as string
 
         if line_info[0] != 'p':                     # if not pline
@@ -31,7 +37,7 @@ for n_id in node_pr.keys():                         # iterate through nodes in p
     prev_pr = graph_data[n_id][0]                   # get last 'curr_pr' from pline
 
     if len(graph_data[n_id]) > 2:                  # if this node has outlinks, print outlinks
-        outlinks = ','.join(graph_data[n_id][2:])       # get comma separated outlinks (without curr_pr, prev_pr)
+        outlinks = graph_data[n_id][2]       # get comma separated outlinks (without curr_pr, prev_pr)
         sys.stdout.write('%s\t%s,%s,%s\n' %(n_id, curr_pr, prev_pr, outlinks))
     else:
         sys.stdout.write('%s\t%s,%s\n' %(n_id, curr_pr, prev_pr))
